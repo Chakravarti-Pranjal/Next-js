079f2d95dc413fc4f3eb1617e643028e68f570bc
chakravarti-pranjal <pranjal.chakravarti@corrintech.com>
2025-10-05 17:21:14 +0530

    Another game app for intercepting routes

---
+++ game-intercepting-app/src/components/GameCard.jsx
@@ -0,0 +1,14 @@
+const GameCard = ({ game }) => {
+  return (
+    <div className="bg-gray-800  text-white p-4 rounded-lg shadow-md">
+      <img
+        src={game.img}
+        alt={game.name}
+        className="w-full h-32 object-cover rounded-md"
+      />
+      <h3 className="mt-2 text-lg font-semibold">{game.name}</h3>
+    </div>
+  );
+};
+
+export default GameCard;
